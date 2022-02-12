6e41fb7803ef57b427c1e7963b2200a9204db1c3
de0 <madecero@protonmail.com>
2022-02-11 19:37:31 -0600

    Add files via upload
    Bring in deleted tweet id, user id, and delete reason into deletedData table of relational database.

---
+++ deletedTweetsdb.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+deletedTweetsdb
+Created on Sat Jan 29 12:35:26 2022
+
+@author: madec
+"""
+
+import os
+import ast
+import sqlite3
+
+#TODO: Change to a local directory you want to store the raw txt files
+os.chdir(r'C:\Users\madec\Documents\de0project')
+
+conn = sqlite3.connect('de0project.db') # open the connection
+cursor = conn.cursor()
+
+deletefile = open('deletedTweets_raw_FINAL.txt', 'r', encoding="utf-8")
+num_lines = sum(1 for line in deletefile)
+print ('number of tweets to review: ' + str(num_lines))
+print ('\n')
+deletefile.close()
+
+deletefile = open('deletedTweets_raw_FINAL.txt', 'r', encoding="utf-8")
+counter = 0
+
+for i in range(num_lines):
+    line = deletefile.readline()
+    tweet = ast.literal_eval(line) # converts the string to a dictionary
+    deleteList = []
+    deleteList.append(tweet['id'])
+    deleteList.append(tweet['user']['id'])
+    deleteList.append(tweet['TwythonError'])
+
+    cursor.execute("INSERT OR IGNORE INTO deletedData VALUES (?, ?, ?);", deleteList)
+    counter += 1
+    print ('number of tweets replicated: ' + str(counter))
+    print ('\n')
+
+deletefile.close()
+conn.commit()   # finalize inserted data
+conn.close()    # close the connection
