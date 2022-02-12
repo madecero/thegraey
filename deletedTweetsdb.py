fbc4ffc8d424ec1ddc711f703b28106b7e930abd
de0 <madecero@protonmail.com>
2022-02-11 19:39:31 -0600

    Update deletedTweetsdb.py

--- deletedTweetsdb.py
+++ deletedTweetsdb.py
@@ -11,7 +11,7 @@ import ast
 import sqlite3
 
 #TODO: Change to a local directory you want to store the raw txt files
-os.chdir(r'C:\Users\madec\Documents\de0project')
+os.chdir(r'INSERT PATH')
 
 conn = sqlite3.connect('de0project.db') # open the connection
 cursor = conn.cursor()
