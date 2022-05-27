d839e883b04b3fd8a2dcfbc63401ff9eacf78b9a
de0 <madecero@protonmail.com>
2022-05-26 20:46:30 -0500

    Add files via upload

---
+++ streamlitWork.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 26 12:30:37 2022
+streamlitWork
+@author: madec
+"""
+
+import streamlit as st
+
+VERSION = "0.1"
+
+st.set_page_config(
+    page_title="TRANSPARENTtwitter",
+    page_icon="twitterbanned.png",
+)
+
+st.title("Welcome to TRANSPARENTtwitter")
