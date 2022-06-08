2365e0f5b887e7fabe94c757c76814ee62fcfa78
de0 <madecero@protonmail.com>
2022-06-08 08:54:51 -0500

    Add files via upload

--- streamlitWork.py
+++ streamlitWork.py
@@ -10,9 +10,15 @@ import streamlit as st
 VERSION = "0.1"
 
 st.set_page_config(
-    page_title="TRANSPARENTtwitter",
+    page_title="CensoredTweets",
     page_icon="twitterbanned.png",
 )
 
 st.title("Below are tweets that have been censored by twitter. Reader discretion is strongly advised.")
 
+tweetfile = open('censoredtweets.txt', 'r', newline='',encoding="utf-8")
+
+for i in tweetfile:
+    st.write(str(i[0]) + '\n')
+
+tweetfile.close()
