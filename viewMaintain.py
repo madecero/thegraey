f9ce1ef19e779d4538e33499592ea1a3f0c82482
de0 <madecero@protonmail.com>
2022-10-20 09:55:49 -0500

    Add files via upload

--- viewMaintain.py
+++ viewMaintain.py
@@ -2,14 +2,14 @@
 """
 Created on Tue Mar  1 15:34:08 2022
 viewMaintain
-@author: de0
+@author: madec
 """
 
 import sqlite3
 import os
 
 #TODO: Change to a local directory you want to store the raw txt files
-os.chdir(r'<INSERT PATH>')
+os.chdir(r'C:\Users\madec\Documents\de0project')
 
 conn = sqlite3.connect('de0project.db') # open the connection
 cursor = conn.cursor()
@@ -30,9 +30,14 @@ SELECT
     TD.ID,
     TD.UserID,
     TD.Text,
-    TD.CreatedAt,
+    UD.Name,
+    UD.ScreenName,
+    replace(TD.CreatedAt, '+0000 ', '') AS CreatedAt,
     DD.deleteReason
-FROM TweetData TD LEFT OUTER JOIN deletedData DD
+FROM TweetData TD
+LEFT OUTER JOIN UserData UD 
+ON TD.UserID = UD.ID
+LEFT OUTER JOIN deletedData DD
 ON TD.ID = DD.tweetID;
 '''
 
