c5e83f645f9b42377402717c87d78ba2e4d0fb77
de0 <madecero@protonmail.com>
2022-10-20 11:49:58 -0500

    Update streamlitWork.py

--- streamlitWork.py
+++ streamlitWork.py
@@ -7,14 +7,24 @@ streamlitWork
 
 import streamlit as st
 
-VERSION = "0.1"
+VERSION = "1.0"
 
 st.set_page_config(
     page_title="CensoredTweets",
     page_icon="twitterBanned.png",
 )
 
-st.title("Censored Tweets! Reader discretion is strongly advised.")
+#SIDEBAR
+with st.sidebar:
+    st.image("twitterBanned.png")
+    st.info(
+        "The tweets you see on this page have been deleted by Twitter for violating the [rules](https://help.twitter.com/en/rules-and-policies/twitter-rules) of their platform\n"
+        "I do not condone hate speech. The purpose of this page is to inform the public of what Twitter is deeming to be unacceptable for thier platform\n"
+        "Reader discretion is strongly advised\n"
+        "Check out the code: [Github](https://github.com/thegraey/deletedTweets)\n"
+        "To learn more, check out my blog: [inthegraey.com](https://inthegraey.com)"
+
+st.title("Censored Tweets!")
 
 tweetfile = open('censoredtweets.txt', 'r', newline='',encoding="utf-8")
 
